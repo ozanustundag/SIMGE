da98ffb2e72cc70ee751c8e01c8491535a4d4296
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-13 11:41:46 +0300

    Value Types and Reference Types

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -6,19 +6,43 @@ namespace Hello_World
     {
         static void Main(string[] args) 
         {
-            //simple data types
+            //int a = -5;
+            //int b = a;
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //b = -10;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //int[] a = { -5 };
+            //int[] b = a;
+
+            //Console.WriteLine(a[0]);
+            //Console.WriteLine(b[0]);
+
+            //b[0] = 1;
+
+            //Console.WriteLine(a[0]);
+            //Console.WriteLine(b[0]);
             int a = 5;
-            uint b = 3;
-            float c = 2.2f;
-            decimal d = 4.2M;
-            bool e = true;
-            double f = 2.5;
-            char g = 's';
-            string o = "ozan";
-            
+            int[] b = { 5 };
+
+            Test(a,b);
+            Console.WriteLine(a + " " + b[0] );
 
 
-          
+
+
+
+
+        }
+
+        static void Test(int x, int[] y)
+        {
+            x = 100;
+            y[0] = 100;
         }
             
     }
