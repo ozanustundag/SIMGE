922ae7a146d4d8bf963e05cb7b55f914373e85b2
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-13 10:24:38 +0300

    Hello_World

---
+++ Hello_World/Hello_World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello_World // nameSpace - used for organization
+{
+    class Program // class - has memembers(such as methods)
+    {
+        static void main(string[] args) // a method - contains a block of code
+        {
+            System.Console.WriteLine("Fully Named"); // fully named
+            Console.WriteLine("Hello World!"); // a statement
+            Console.WriteLine("Ozan Ustundag"); // a statement
+
+            // name space -> class -> members -> method -> statement
+        }
+    }
+}
