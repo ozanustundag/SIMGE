22b6e6d6d147d670a7d49ab6b2b13133cfc1a219
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-13 10:37:34 +0300

    Command_Line_Arguments

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -4,13 +4,19 @@ namespace Hello_World // nameSpace - used for organization
 {
     class Program // class - has memembers(such as methods)
     {
-        static void main(string[] args) // a method - contains a block of code
+        static void Main(string[] args) // a method - contains a block of code
+        {
+            Program myProgram = new Program();
+            myProgram.Print();
+            Console.WriteLine(args[0]);
+            // name space -> class -> members -> method -> statement
+        }
+         void Print()
         {
             System.Console.WriteLine("Fully Named"); // fully named
             Console.WriteLine("Hello World!"); // a statement
             Console.WriteLine("Ozan Ustundag"); // a statement
-
-            // name space -> class -> members -> method -> statement
         }
+        
     }
 }
