1c6b3a30d3938effa689e38eb36c63de959a7761
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-13 10:40:32 +0300

    User_Input

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -1,22 +1,15 @@
 ﻿using System;
 
-namespace Hello_World // nameSpace - used for organization
+namespace Hello_World 
 {
-    class Program // class - has memembers(such as methods)
+    class Program 
     {
-        static void Main(string[] args) // a method - contains a block of code
+        static void Main(string[] args) 
         {
-            Program myProgram = new Program();
-            myProgram.Print();
-            Console.WriteLine(args[0]);
-            // name space -> class -> members -> method -> statement
+            string name = Console.ReadLine();
+            Console.WriteLine(name + " 18");
+          
         }
-         void Print()
-        {
-            System.Console.WriteLine("Fully Named"); // fully named
-            Console.WriteLine("Hello World!"); // a statement
-            Console.WriteLine("Ozan Ustundag"); // a statement
-        }
-        
+            
     }
 }
