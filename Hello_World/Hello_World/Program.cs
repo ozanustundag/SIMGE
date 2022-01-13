1b1fef75c857cb56833c9d356a76a549ec8a2d75
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-13 15:56:53 +0300

    Generic Lists

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Hello_World
 {
@@ -7,47 +8,17 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            int[,] grades = new int[2, 3]
-            {
-                {2,4,5 },
-                {4,84,95}
-            };
-            int[,] grads2 =
-            {
-                {4,5,5,5 },
-                {4,8,9,6 },
-                {1,4,6,8 }
-            };
-            int[][] grads3 =
-            {
-                new int[] {4,7,8},
-                new int[] { 1},
-                new int[] {2,3}
-            };
+            //Lists
 
-            Console.WriteLine(grads3.Length);
-            Console.WriteLine(grads3[1].Length);
-
-            for (int i = 0; i < grads3.Length; i++)
-            {
-                for (int k = 0; k < grads3[i].Length; k++)
-                {
-                    Console.Write(grads3[i][k] +" ");
-                }
-                Console.WriteLine();
-            }
-
-            Console.WriteLine(grades.GetLength(0));
-            Console.WriteLine(grades.GetLength(1));
-
-            for (int i = 0; i < grades.GetLength(0); i++)
-            {
-                for (int k = 0; k < grades.GetLength(1); k++)
-                {
-                    Console.Write(grades[i,k] + " ");
-                }
-                Console.WriteLine();
-            }
+             List<int> grades = new List<int>() {5,10 };
+           
+            grades.Add(50);
+            grades.Insert(2, 14);
+            //grades.Clear();
+            grades.Remove(2);
+            Console.WriteLine(grades[2]);
+            Console.WriteLine(grades[3]);
+             
         }
     }
 }   
