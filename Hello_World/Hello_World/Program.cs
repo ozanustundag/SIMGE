ad99f29d9183018f8193c2a6e2bb1411662d0879
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-13 13:47:18 +0300

    If, else if, else

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -11,8 +11,25 @@ namespace Hello_World
             ozan.FirstName = "ozan";
             ozan.SurName = "ustundag";
 
-            Console.WriteLine(ozan.GetFullName());
-            Console.WriteLine(ozan.FullName);
+            //Console.WriteLine(ozan.GetFullName());
+            //Console.WriteLine(ozan.FullName);
+            Console.WriteLine("Guess name: ");
+            string guess1 = Console.ReadLine();
+            Console.WriteLine("second guess for name:");
+            string guess2 = Console.ReadLine();
+
+            if (guess1 == ozan.FullName)
+            {
+                Console.WriteLine("+1 point");
+            }
+            else if (guess2 == ozan.FullName)
+            {
+                Console.WriteLine("+1/2 point ");
+            }
+            else
+            {
+                Console.WriteLine("0 point");
+            }
         }           
     }
 }
