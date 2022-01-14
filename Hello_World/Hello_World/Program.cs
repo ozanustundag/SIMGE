1fde0fc85c4681e245de9306ef4effaaab232bcf
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-14 10:30:18 +0300

    Convert Between List and Array

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -8,36 +8,23 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            //nested foreach loop
-
-            List<List<int>> studentGrades = new List<List<int>>() {
-            new List<int> {5,10 },
-            new List<int>{26,45,54},
-            new List<int>{1},
-            new List<int>{68,68,26}
-            };
-            foreach (List<int> grade in studentGrades)
+            //convert between list and array
+            List<string> movies = new List<string> { "pulp fiction", "dark knight rises" };
+            string[] myArray = movies.ToArray();
+            foreach (string movie in myArray)
             {
-                foreach (int grades in grade)
-                {
-                    Console.WriteLine(grades + "\t");
-                }
-                Console.WriteLine();    
+                Console.WriteLine(movie + "\n");
             }
-
-            int[][] studentGrades2 = {
-               new int[] {2,4},
-                new int [] {3,4,6},
-                new int [] {4,9,45 }
-            };
-
-            foreach (int[] grade in studentGrades2)
+            int[][] myLuckyNumbers =
             {
-                foreach (int grades in grade)
-                {
-                    Console.WriteLine(grades+" ");
-                }
-                Console.WriteLine();
+               new int[] {5,2,8,9},
+               new int[] {45,88,78},
+               new int[] {4,7}
+            };
+            List<int> myNewList = myLuckyNumbers[1].ToList();
+            foreach (int numbers in myNewList)
+            {
+                Console.WriteLine(numbers + "\n");
             }
         }
     }
