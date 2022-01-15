a4ad7f6fbbd430230b746b6af213bf382f8e07df
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-15 17:07:26 +0300

    Custom Types as Arguments

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -7,26 +7,35 @@ namespace Hello_World
     class Program
     {
         static void Main(string[] args)
-        {
-            // creating a class and instantiating an object
-
-            Program myProgram = new Program();//instantiate
-            myProgram.DoSomething();        
+        { 
+            // using custom types as arguments
+            Program myProgram = new Program();
+            //myProgram.DoSomething();
+            User ozan = new User();
+            ozan.Name = "ozan";
+            ozan.SurName = "karadağ";
+            myProgram.TakeUser(ozan);
+            Console.WriteLine(ozan.FullName);
         }
         public void DoSomething()
         {
             List<string> firstNames = new List<string>() { "ozan", "kadir", "ömer" };
             List<string> lastNNames = new List<string>() { "karadağ", "belçika", "kanada" };
-
+            List<User> users = new List<User>();
             for (int i = 0; i <firstNames.Count; i++)
             {
                 User user = new User();
                 user.Name = firstNames[i];
                 user.SurName = lastNNames[i];
-                Console.WriteLine(user.FullName);
+                users.Add(user);
+                //Console.WriteLine(user.FullName);
             }
-           
-
+        }
+        public void TakeUser(User user)
+        {
+            user = new User();
+            user.Name = "selami";
+            Console.WriteLine(user.FullName);
         }
     }
 }   
