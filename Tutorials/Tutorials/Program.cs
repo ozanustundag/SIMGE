5b73ad47a5a829223ee0576e1ce93fa7f8c7de8e
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-17 11:11:09 +0300

    Nullable Types

---
+++ Tutorials/Tutorials/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //nuallable types
+
+            int? x = null;
+            Program program = null;
+            bool? y = null;
+
+        }
+    }
+}
