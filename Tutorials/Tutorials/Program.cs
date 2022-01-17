9960f506fb552ccb5d953b2adc21cab08674d01c
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-17 14:29:51 +0300

    Sealed Class

--- Tutorials/Tutorials/Program.cs
+++ Tutorials/Tutorials/Program.cs
@@ -1,76 +1,12 @@
 ﻿using System;
 using System.Data;
-using PATA = ProjectA.TeamA;
-using PBTA= ProjectB.TeamA;
 
 namespace Tutorials
 {
-    class Program
+    class Program : People
     {
-        static void Main(string[] args)
-        {
-            //NameSpace
+        //sealed class başka bir sınıfa miras verilemez
 
-            PATA.A a = new PATA.A();
-            a.Print();
-        }
-    }
-}
 
-namespace ProjectA
-{
-    namespace TeamA
-    {
-        class A
-        {
-            public void Print()
-            {
-                Console.WriteLine("Project A - TEAM A - CLASS A");
-            }
-        }
-        class B
-        {
-            public void Print()
-            {
-                Console.WriteLine("Project A - TEAM A - CLASS B");
-            }
-        }
-    }
-    namespace TeamB
-    {
-        class A
-        {
-            public void Print()
-            {
-                Console.WriteLine("Project A - TEAM B - CLASS A");
-            }
-        }
-        class B
-        {
-            public void Print()
-            {
-                Console.WriteLine("Project A - TEAM A - CLASS B");
-            }
-        }
-    }
-}
-namespace ProjectB
-{
-    namespace TeamA
-    {
-        class A
-        {
-            public void Print()
-            {
-                Console.WriteLine("Project B - TEAM A - CLASS A");
-            }
-        }
-        class B
-        {
-            public void Print()
-            {
-                Console.WriteLine("Project B - TEAM A - CLASS B");
-            }
-        }
     }
 }
