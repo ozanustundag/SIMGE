0be59a3ea08f894869cccda4e7267bf1c1e644e2
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-17 11:34:15 +0300

    Ref and Out Parameters

--- Tutorials/Tutorials/Program.cs
+++ Tutorials/Tutorials/Program.cs
@@ -6,12 +6,37 @@ namespace Tutorials
     {
         static void Main(string[] args)
         {
-            //nuallable types
+            //ref and out parameters
+            // bu parametreler value type ların değerlerinin metot içerisinde değiştirilmesine yarıyor.
 
-            int? x = null;
-            Program program = null;
-            bool? y = null;
+            int number = 5;
+            int numberOut ;
 
+            Console.WriteLine("Before Param:" + number);
+            Param(number);
+            Console.WriteLine("Afer Param:" + number);
+            //--------------------------------------------------
+            Console.WriteLine("Before ParamOut:" + 0);
+            ParamOut(out numberOut);
+            Console.WriteLine("Afer ParamOut:" + numberOut);
+            //--------------------------------------------------
+            Console.WriteLine("Before ParamRef:" + numberOut);
+            ParamRef(ref numberOut);
+            Console.WriteLine("Afer ParamRef:" + numberOut);
+
+        }
+
+        static void Param(int x)
+        {
+            x = x * 2;
+        }
+        static void  ParamOut(out int x)
+        {
+            x = 2; // out değer atamayı zorunlu kılıyor meteto içerisinde.
+        }
+        static void ParamRef(ref int x)
+        {
+            x = x+ 8;
         }
     }
 }
