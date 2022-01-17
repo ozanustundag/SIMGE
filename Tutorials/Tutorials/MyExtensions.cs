3e7735110462ea783bf69f6e2736f3c4904ebe81
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-17 21:36:52 +0300

    Extension Methods

---
+++ Tutorials/Tutorials/MyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tutorials
+{
+    public static class MyExtensions
+    {
+
+        public static void DisplayASMInfo(this object obj)
+        {
+            Console.WriteLine("heloooooo");
+
+        }
+        public static int Squareee(this int x)
+        {
+           return x*x;
+        }
+
+        public static void ShowList(this IEnumerable iterator)
+        {
+            foreach (var item in iterator)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
--- Tutorials/Tutorials/Program.cs
+++ Tutorials/Tutorials/Program.cs
@@ -7,60 +7,22 @@ using System.Linq;
 
 namespace Tutorials
 {
-    class Program 
+    class Program
     {
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
+            //Extension Methods
 
-            //  Dynamic Programming
+            object obj = new object();
+            obj.DisplayASMInfo();
+            int a = 5;
+            Console.WriteLine( a.Squareee());
+            List<string> list = new List<string>() { "ali0", "veli", "mehmet" };
+            list.ShowList();
 
-            //dynamic a = 5;
-            //Console.WriteLine(a);
-            //a = "aa";
-            //Console.WriteLine(a);
+            
 
-            //dynamic people = new ExpandoObject();
-            //people.Name = "Ozi";
-            //people.SurName = "Üstündağ";
-            //Console.WriteLine(people.Name + " " + people.SurName);
-
-            dynamic people = new ExpandoObject();
-            people.Name = "Ozark";
-            people.SurName = "kalander";
-            people.ShowName = new Action(() =>
-            {
-                Console.WriteLine("Name: " + people.Name); 
-            }
-            );
-            people.Repeat = new Func<string, string>(input =>
-             {
-                 return input;
-             });
-            //Çalıştırma işlemleri
-            //people.ShowName();
-
-            //dynamic result = people.Repeat("multi repeat");
-            //Console.WriteLine(result);
-            foreach (dynamic item in GetList())
-            {
-                Console.WriteLine("{0}{1}",item.Name,item.Idd );
-            }
 
         }
-
-        private static IEnumerable<dynamic> GetList()
-        {
-            List<User> users = new List<User> { new User() { FullName = "ozan Üstündağ", Id = 99 },
-            new User() { FullName = "kamil Üstündağ", Id = 99 }};
-            return from k in users
-                   select new {Name=k.FullName, Idd=k.Id};
-        }
-        
-     
-    }
-    class User
-    {
-        public string FullName { get; set; }
-        public int Id { get; set; }
     }
 }
