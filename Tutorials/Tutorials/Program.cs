4975352dec6dbcef225e71ead441376d2792e125
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-17 13:18:58 +0300

    Exception Handling

--- Tutorials/Tutorials/Program.cs
+++ Tutorials/Tutorials/Program.cs
@@ -1,49 +1,62 @@
 ﻿using System;
-
+using System.Data;
 namespace Tutorials
 {
-    enum Acces
-    {
-        Admin = 1, Moderator = 5, User = 8
-    }
-    enum ShoesBrand
-    {
-        Adidas=50, Nike=250, Nixe=185
-    }
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            /*c# dilinde enum yapısı. Bu yapı yazılım dilinde enum, enumaration ya da enum sabitleri olarak
-             adlandırılır. Değişkenlerin alabilceği değerlerin sabit(belli) olduğu durumlarda programı
-            daha okunabilir hale getirmke için kullanılır. Programda birçok değişkene tek tek değer vermek yerine 
-            enum kullanılabilir.
+            //Exception Handling
+
+            int a = 8;
+            int b = 0;
+            /*  try
+              {
+                  //hata oluşabilecek kodların yazıldığı alan
+                  Console.WriteLine(a / b);
+              }
+              catch (Exception)
+              {
+                  // hata oluşunca yaılacakların yazıldığı alan
+                  Console.WriteLine("Error");
+
+              }
             */
-            //Enum
+            int[] numbers = new int[3] { 5, 4, 6 };
+            /*  try
+              {
+                  Console.WriteLine(numbers[5]);
+                  Console.WriteLine(a / b);
+              }
+              catch (IndexOutOfRangeException ex)
+              {
+                  Console.WriteLine(ex.Message);
+              }
+              catch(DivideByZeroException ex)
+              {
+                  Console.WriteLine(ex.Message);
+              }
+              catch(Exception ex)
+              {
+                  Console.WriteLine(ex.Message);
+              } */
 
-            Shoes shoes = new Shoes();
-            shoes.SelectBrand = ShoesBrand.Adidas;
-            shoes.SearchByBrand(ShoesBrand.Adidas);
+            try
+            {
 
-            
+            }
+            catch (Exception)
+            {
 
-        }    
+                throw;
+            }
+            finally
+            {
+                //hata olsa da olmasa da çalışır. hata olduğunda halledeilmesi gereken son işlemler burada yapılır
+                //ve hata fırlatılır.
+            }
+        }     
     }
-     class Shoes
-     {
-        public ShoesBrand SelectBrand { get; set; }
-        public  void SearchByBrand(ShoesBrand shoes)
-        {
-            if (shoes== SelectBrand)
-            {
-                Console.WriteLine("Shoes Matched");
-            }
-            else
-            {
-                Console.WriteLine("Shoes dont matched");
-            }
-        }
-     }
-
-
 }
