a51ccd65b2b112981d0f0d03ec5262fd95234329
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-19 16:04:19 +0300

    Queue

---
+++ UdemyTutorials1/UdemyTutorials1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UdemyTutorials1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary 
+            Dictionary<int, string> myDic = new Dictionary<int, string>() { {1, "ozan" },{ 5,"Aslı"} };
+
+            foreach (var x in myDic)
+            {
+                Console.WriteLine(x.Key + "=" + x.Value);
+            }
+            //Queue
+            Queue<string> myQue = new Queue<string>();
+            myQue.Enqueue("ankara");
+            myQue.Enqueue("izmir");
+            for (int i = 0; i < myQue.Count; i++)
+            {
+                Console.WriteLine(myQue.Dequeue());
+            }
+
+        }
+    }
+}
