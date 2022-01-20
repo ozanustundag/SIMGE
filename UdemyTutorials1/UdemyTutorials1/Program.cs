e1cb6324b22e82a5eff3f7756c62d60401301d2c
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-20 10:48:17 +0300

    Ref and Out Keywords

--- UdemyTutorials1/UdemyTutorials1/Program.cs
+++ UdemyTutorials1/UdemyTutorials1/Program.cs
@@ -8,18 +8,31 @@ namespace UdemyTutorials1
     {
         static void Main(string[] args)
         {
-            // Params Keyword
+            // Ref and Out Keywords
+            // ref ilk değer bekler out beklemez
+            int a = 5;
+            int b=1;
+            Console.WriteLine(Sum(a, b));
+            Console.WriteLine("After Normal Sum Method:");
+            Console.WriteLine("a=" + a + "b=" + b);
+            Console.WriteLine(Sum(ref a, out b));
+            Console.WriteLine("After our/ref method:");
+            Console.WriteLine("a=" + a + "b=" + b);
 
-            Console.WriteLine(Sum(4,8,99,8));
+
         }
-        static int Sum(params int[] numbers)
+      static int Sum(ref int number1,out int number2)
         {
-            int result = 0;
-            foreach (var item in numbers)
-            {
-                result += item;
-            }
-            return result;
+            number1 = 1;
+            number2 = 2;
+            return number2 + number1;
         }
+        static int Sum( int number1, int number2)
+        {
+            number1 = 1;
+            number2 = 2;
+            return number2 + number1;
+        }
+
     }
 }
