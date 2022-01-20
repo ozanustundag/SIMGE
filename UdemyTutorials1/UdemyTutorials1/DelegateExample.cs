1584cd1270baa02c0d43f080cbe778e6c7edef31
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-20 13:11:44 +0300

    Delegates

---
+++ UdemyTutorials1/UdemyTutorials1/DelegateExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyTutorials1
+{
+    class DelegateExample
+    {
+        public delegate void NoneInputDelegates();
+        public delegate void TwoInputDelegates(int x,int y);
+
+        public void Start()
+        {
+            NoneInputDelegates noneInputDelegates = new NoneInputDelegates(Method1);
+            noneInputDelegates += Method2;
+
+            TwoInputDelegates twoInputDelegates = new TwoInputDelegates(Method3);
+            twoInputDelegates += Method4;
+
+            noneInputDelegates?.Invoke();
+            twoInputDelegates(5, 4);
+        }
+        public void Method1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Method 2");
+        }
+        public void Method3(int x, int y)
+        {
+            Console.WriteLine("Method 3");
+        }
+        public void Method4(int x,int y)
+        {
+            Console.WriteLine("Method 4");
+        }
+    }
+}
--- UdemyTutorials1/UdemyTutorials1/Program.cs
+++ UdemyTutorials1/UdemyTutorials1/Program.cs
@@ -8,22 +8,9 @@ namespace UdemyTutorials1
     {
         static void Main(string[] args)
         {
-            //Static
-            Product product = new Product();
-            Product product1 = new Product();
-            Console.WriteLine(Product.StaticMethod());
-            Console.WriteLine(product.normalMethod());
-            Console.WriteLine(product1.normalMethod());
 
-            Product product2 = new Product();
-            Product product3 = new Product();
-            Console.WriteLine(product2.normalMethod());
-            Console.WriteLine(product3.normalMethod());
-
-            Console.WriteLine(Product.StaticMethod());
-
-
-        //    Static_Product static_Product = new Static_Product();//static class ın nesne örneğini oluşturamam.
+            DelegateExample delegateExample = new DelegateExample();
+            delegateExample.Start();
 
         }
     }
