3d61346d01b1a892437fa88832be4b9b9358a36f
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-20 14:21:42 +0300

    Git Ignore and Events

---
+++ Udemy Tutorial Project/Assets/Scripts/Events.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class Events : MonoBehaviour
+{
+    //public delegate void myDelegate();
+    //public event myDelegate myEvent;
+    public event Action myEvent;
+    [SerializeField] UnityEvent unityEvent;
+    int x;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    void Awake()
+    {
+       
+    }
+    void OnEnable()
+    {
+        myEvent += Method1;
+        myEvent();
+        unityEvent.Invoke();
+    }
+    private void OnDisable()
+    {
+        myEvent -= Method1;
+    }
+
+    private void Method1()
+    {
+        Debug.Log("Event Gerçekliþti");
+    }
+    public void Method2()
+    {
+        Debug.Log("Unity Event Gerçekliþti");
+    }
+    public void dkaþslf()
+    {
+
+    }
+}
